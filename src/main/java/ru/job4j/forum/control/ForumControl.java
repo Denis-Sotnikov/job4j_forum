d2eef84385f8909bef59ec2f44d8e2de5aae61eb
Denis Sotnikov <sport_piter@mail.ru>
2021-03-12 03:44:13 +0300

    0. Spring Boot [#469162]v

---
+++ src/main/java/ru/job4j/forum/control/ForumControl.java
@@ -0,0 +1,52 @@
+package ru.job4j.forum.control;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.job4j.forum.service.PostService;
+
+@Controller
+public class ForumControl {
+    private final PostService posts;
+
+    public ForumControl(PostService posts) {
+        this.posts = posts;
+    }
+
+////    @ModelAttribute Accident accident
+//    @PostMapping("/save")
+//    public String save(@RequestParam("rIds") List<String> rules) { //,@ModelAttribute Accident accident) {
+////        serviceHbm.saveOr(accident, rules);
+////        springData.save(accident, rules);
+//        return "redirect:/";
+//    }
+
+    @GetMapping("/edit")
+    public String editPost(Model model) {
+        model.addAttribute("posts", posts.getAll());
+        return "post/edit";
+    }
+
+    @GetMapping("/create")
+    public String createPost(Model model) {
+        model.addAttribute("posts", posts.getAll());
+        return "post/create";
+    }
+
+    @GetMapping("/topics/create")
+    public String createTopic(Model model) {
+        model.addAttribute("posts", posts.getAll());
+        return "topics/create";
+    }
+
+    @GetMapping("/topics/edit")
+    public String editTopic(Model model) {
+        model.addAttribute("posts", posts.getAll());
+        return "topics/edit";
+    }
+
+
+}
+
