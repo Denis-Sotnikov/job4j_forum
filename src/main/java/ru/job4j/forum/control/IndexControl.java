5dd07bb6edda7ddfb751fc2e27dc08eb37b3e550
Denis Sotnikov <sport_piter@mail.ru>
2021-03-13 04:33:55 +0300

    Controlls

--- src/main/java/ru/job4j/forum/control/IndexControl.java
+++ src/main/java/ru/job4j/forum/control/IndexControl.java
@@ -1,22 +1,29 @@
 package ru.job4j.forum.control;
 
-
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import ru.job4j.forum.service.PostService;
+import ru.job4j.forum.model.User;
+import ru.job4j.forum.service.ForumService;
 
 @Controller
 public class IndexControl {
-    private final PostService posts;
 
-    public IndexControl(PostService posts) {
-        this.posts = posts;
+    private final ForumService fService;
+
+    public IndexControl(ForumService fService) {
+        this.fService = fService;
     }
 
     @GetMapping({"/", "/index"})
     public String index(Model model) {
-        model.addAttribute("posts", posts.getAll());
+        model.addAttribute("topics", fService.getAllTopics());
+        model.addAttribute("user", SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal()
+        );
         return "index";
     }
 }
--- src/main/java/ru/job4j/forum/control/LoginControl.java
+++ src/main/java/ru/job4j/forum/control/LoginControl.java
@@ -1,9 +1,15 @@
 package ru.job4j.forum.control;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class LoginControl {
@@ -20,5 +26,14 @@ public class LoginControl {
         }
         model.addAttribute("errorMessage", errorMessage);
         return "login";
+    }
+
+    @GetMapping(value = "/logout")
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout=true";
     }
 }
