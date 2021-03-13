64fa6545c147acd540db51d8ab3a109ef0355738
Denis Sotnikov <sport_piter@mail.ru>
2021-03-13 04:40:30 +0300

    Repositories

--- src/main/java/ru/job4j/forum/control/RegControl.java
+++ src/main/java/ru/job4j/forum/control/RegControl.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.job4j.forum.model.User;
-import ru.job4j.forum.repository.AuthorityRepository;
-import ru.job4j.forum.repository.UserRepository;
 
 @Controller
 public class RegControl {
