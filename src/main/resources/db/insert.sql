36ab9fe44686bd1dc78eb79437174c19ae1395f7
Denis Sotnikov <sport_piter@mail.ru>
2021-03-21 05:12:39 +0300

    update

--- src/main/resources/db/insert.sql
+++ src/main/resources/db/insert.sql
@@ -1,2 +1,6 @@
+insert into authorities(authority) values ('ROLE_USER');
+insert into authorities(authority) values ('ROLE_ADMIN');
+insert into users(username,enabled, password, authority_id) values ('root', true, '$2a$10$wY1twJhMQjGVxv4y5dBC5ucCBlzkzT4FIGa4FNB/pS9GaXC2wm9/W', 2);
+
 insert into posts (name) values ('О чем этот форум?');
 insert into posts (name) values ('Правила форума.');
